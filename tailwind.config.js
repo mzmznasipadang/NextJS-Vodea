979f04f0022719b6b35203be84a4882f8ae84208
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2022-08-05 11:14:50 +0700

    *uhuk* Updated Global css for Tailwind Usages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',,
+    './pages/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',,
   ],
   theme: {
     colors: {
       background : '#F5FAFE',
       primary: '#001233', //Text Color
-      secondary: '#0078F0'
+      secondary: '#0078F0',
+      neutral: { //INI SHADES PUTIH KE GREY 
+          100: '#FFFFFF',
+          200: '#F6F6F6',
+          300: '#EDEDED',
+          400: '#DFDFDF'
+      }
+      
     },
     extend: {},
   },
