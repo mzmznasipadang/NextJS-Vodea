c1f49da974ec5b3d6283bb109124cff677e42dc7
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2022-08-05 11:04:13 +0700

    updated tailwind config and index.js

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Victor Chandra</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hello there, My name is Victor and this is the fudamental build with documentation for{' '}
+          <a href="https:/vodea.id">Vodea New Web</a>.
         </p>
       </section>
     </Layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',,
   ],
   theme: {
+    colors: {
+      background : '#F5FAFE',
+      primary: '#001233', //Text Color
+      secondary: '#0078F0'
+    },
     extend: {},
   },
   plugins: [],
